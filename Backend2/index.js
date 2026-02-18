dd020413b8f4203b8328fb0bf0da7381c61a3fe1
Its-Steve19 <stevecodehub@gmail.com>
2026-02-18 17:54:52 +0300

    modified imprting ans exporting

--- Backend2/index.js
+++ Backend2/index.js
@@ -1,4 +1,13 @@
-const math = require("./math");
+const math = require("./math")
 
-const result = math.add(5, 3);
-console.log("Result:", result);
+const result1 = math.add(10, 3);
+console.log("Result: ", result1);
+
+const result2 = math.multiply(10, 3);
+console.log("Result: ", result2);
+
+const result3 = math.divide(10, 3);
+console.log("Result: ", result3);
+
+const result4 = math.modulus(10, 3);
+console.log("Result: ", result4);
--- Backend2/math.js
+++ Backend2/math.js
@@ -1,4 +1,17 @@
 function add(a, b) {
     return a + b;
 }
-module.exports = {add};   
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
+
+module.exports = {add,multiply,divide,modulus};
